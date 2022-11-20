00bccb90d757e838dbfc40c04fbe0d6f451f3bf8
Daniel M. Sahu <danielmohansahu@gmail.com>
2022-11-20 13:58:22 -0500

    Added a few more tests; basic restructuring.

--- tests/test_io.py
+++ tests/test_io.py
@@ -4,6 +4,9 @@
 # pytest
 import pytest
 
+# YAML
+import yaml
+
 # validir
 from validir.template import Template
 
@@ -45,9 +48,28 @@ def test_simple_load_bad():
 
 def test_load():
     # make sure we succeed when loading this
-    with open("sample_template_1.yaml", "r") as yamlfile:
+    with open("samples/sample_template_1.yaml", "r") as yamlfile:
         try:
             Template(yamlfile)
         except Exception as e:
             assert False, f"Load failure: {e}"
 
+def test_transitive():
+    # make sure we can load from and dump to file without changing anything
+     
+    with open("samples/sample_template_1.yaml", "r") as yamlfile:
+        stream = yamlfile.read()
+
+    # save nominal yaml load
+    original = yaml.safe_load(stream)
+
+    # make sure we can actually parse this!
+    try:
+        t = Template(stream)
+    except Exception as e:
+        assert False, f"Load failure: {e}"
+
+    # compare parsed version to original
+    parsed = t.dump()
+    assert (parsed["root"] == original["root"])
+    
