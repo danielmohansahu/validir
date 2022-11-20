676189b884d39198600b0b0121743dbc45f93d0e
Daniel M. Sahu <danielmohansahu@gmail.com>
2022-11-20 10:53:29 -0500

    Fleshing out basic unit tests.

--- tests/test_io.py
+++ tests/test_io.py
@@ -2,15 +2,52 @@
 """
 
 # pytest
-import unittest
+import pytest
 
 # dir_validate
-# from dir_validate import Template
+from dir_validate.template import Template
 
-# content of test_sample.py
-def inc(x):
-    return x + 1
+# define some valid and invalid documents
+simple_valid_docs = [
+"""root:
+    - foo
+""",
+"""root:
+    - foo:
+        - bar1
+"""
+]
+simple_invalid_docs = [
+"""root:
+    foo:
+        bar
+""",
+"""foo:
+    - bar
+""",
+"""foo:
+"""
+]
 
-def test_answer():
-    assert inc(3) == 4
+def test_simple_load():
+    # make sure we succeed when loading these
+    for doc in simple_valid_docs:
+        try:
+            Template(doc)
+        except Exception as e:
+            assert False, f"Load failure: {e}"
+
+def test_simple_load_bad():
+    # make sure we fail when loading these
+    for doc in simple_invalid_docs:
+        with pytest.raises(Exception):
+            Template(doc)
+
+def test_load():
+    # make sure we succeed when loading this
+    with open("sample_template_1.yaml", "r") as yamlfile:
+        try:
+            Template(yamlfile)
+        except Exception as e:
+            assert False, f"Load failure: {e}"
 
