4a29fda55cb1fc92ace6a9814ebabf471c8d290a
Daniel M. Sahu <danielmohansahu@gmail.com>
2022-11-21 18:41:39 -0500

    Added a test suite (of 1) to test generation of templates.

---
+++ tests/test_generate.py
@@ -0,0 +1,37 @@
+""" Unit tests for verification of template generation from data.
+
+These tests ensure we:
+ - Generate expected template files from sample directories.
+"""
+
+# STL
+import os
+import glob
+
+# pytest
+import pytest
+
+# YAML
+import yaml
+
+# validir
+from validir.template import Template
+
+# test directories and the location of ground truth data
+DIRECTORIES = ["directories/extra_directory", "directories/missing_directory", "directories/simple_directory"]
+TRUTH_DIR = "directories/generated_templates"
+
+############################ TEST FUNCTIONS ###################################
+
+def test_generation():
+    # iterate through all test directories
+    for directory in DIRECTORIES:
+        # generate templates with a variety of conditions
+        template_hidden = Template.construct(directory, skip_hidden=False)
+        template_no_hidden = Template.construct(directory, skip_hidden=True)
+        
+        # compare to "ground truth"
+        with open(os.path.join(TRUTH_DIR, directory + "_hidden.yaml", "r")) as yamlfile:
+            assert yaml.safe_load(yamlfile)["root"] == template_hidden.dump()["root"]
+        with open(os.path.join(TRUTH_DIR, directory + "_no_hidden.yaml", "r")) as yamlfile:
+            assert yaml.safe_load(yamlfile)["root"] == template_no_hidden.dump()["root"]
