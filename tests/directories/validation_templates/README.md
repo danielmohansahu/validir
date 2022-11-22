0d41510650c1ddd5be4f4ddf75b9a35273f736c2
Daniel M. Sahu <danielmohansahu@gmail.com>
2022-11-21 19:04:11 -0500

    Added a third and final set of tests (regression) for end-to-end testing.

---
+++ tests/directories/validation_templates/README.md
@@ -0,0 +1,1 @@
+Each directory in this folder contains valid and invalid templates for the corresponding "example" directory.
---
+++ tests/test_validate.py
@@ -0,0 +1,39 @@
+""" Regression tests for full validation of directories against templates.
+
+These tests ensure full end-to-end operation is as expected.
+"""
+
+# STL
+import os
+import glob
+
+# pytest
+import pytest
+
+# YAML
+import yaml
+
+# validir
+from validir.template import Template
+
+# test directories and the corresponding templates
+DIRECTORIES = ["directories/extra_directory", "directories/missing_directory", "directories/simple_directory"]
+TEMPLATES = "directories/validation_templates"
+
+############################ TEST FUNCTIONS ###################################
+
+def test_validation():
+    # iterate through all test directories
+    for directory in DIRECTORIES:
+        # find the corresponding templates
+        valid_templates = glob.glob(os.path.join(TEMPLATES, directory, "valid", "*.yaml"))
+        invalid_templates = glob.glob(os.path.join(TEMPLATES, directory, "invalid", "*.yaml"))
+        
+        # make sure the valid templates succeed
+        for template in valid_templates
+            assert (Template(template).validate(directory))
+
+        # make sure the invalid templates fail
+        for template in invalid_templates:
+            assert (not Template(template).validate(directory))
+
